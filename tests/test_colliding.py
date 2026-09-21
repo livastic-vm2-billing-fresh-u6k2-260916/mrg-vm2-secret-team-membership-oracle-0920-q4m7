c201c49137dea76309030a3f191c7a215526337a
VM2 Secondary <onecursor3+livastic+vm2+secondary+20260911@gmail.com>
2026-09-22 04:09:13 +0900

    cleanup controlled latest pytest JUnit collision chain proof

--- tests/test_colliding.py
+++
@@ -1,4 +0,0 @@
-def test_colliding(record_xml_attribute):
-    record_xml_attribute("classname", "pkg.A.B")
-    record_xml_attribute("name", "test")
-    assert False, "different contributor regression on latest"
--- tests/test_intended.py
+++
@@ -1,4 +0,0 @@
-def test_intended(record_xml_attribute):
-    record_xml_attribute("classname", "pkg.A")
-    record_xml_attribute("name", "B.test")
-    assert True
